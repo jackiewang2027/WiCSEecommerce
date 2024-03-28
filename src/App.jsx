ed0db96e880b432304e6bd9a087b70165de89cce
Jackie <jackiewang@ufl.edu>
2024-03-28 19:29:44 -0400

    collections finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route} from "react-router-dom"
 import Home from './routes/Home'
 import Navbar from './components/Navbar'
+import Collections from './routes/Collections'
 
 function App() {
 
@@ -10,10 +11,12 @@ function App() {
     <>
      <Navbar />
      <Routes>
-        <Route path="/" element={<Home />} />
+         <Route path="/" element={<Home />} />
+         <Route path="/collection/:name" element={<Collections/>} />
      </Routes>
+     
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/collectionData.js
@@ -0,0 +1,10 @@
+const Collections = {
+    tops: [
+        {id: 1, name: "Jules Top", description: "a", imagePath:"/public/tops/top1.webp", price: 32.00},
+        {id: 2, name: "Maria Top", description: "a", imagePath:"/public/tops/top2.webp", price: 30.00},
+        {id: 3, name: "Paulina Top", description: "a", imagePath:"/public/tops/top3.webp", price: 31.00},
+        {id: 4, name: "Riley Top", description: "a", imagePath:"/public/tops/top4.webp", price: 34.00},
+    ]
+};
+
+export default Collections;
